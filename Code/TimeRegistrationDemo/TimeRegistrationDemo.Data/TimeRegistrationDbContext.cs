825773d16e31922372d2cdbc6ac465f00cc2effc
Filip Nuyts <filipnuyts@skynet.be>
2018-03-06 22:34:34 +0100

    migrations is enabled

--- Code/TimeRegistrationDemo/TimeRegistrationDemo.Data/TimeRegistrationDbContext.cs
+++ Code/TimeRegistrationDemo/TimeRegistrationDemo.Data/TimeRegistrationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Configuration;
-using System.Diagnostics;
 using TimeRegistrationDemo.Data.Entities;
 
 namespace TimeRegistrationDemo.Data
 {
     public class TimeRegistrationDbContext : DbContext
     {
+        public TimeRegistrationDbContext(DbContextOptions options) : base(options)
+        { }
+
         public DbSet<UserEntity> Users { get; set; }
         public DbSet<HolidayRequestEntity> HolidayRequests { get; set; }
         public DbSet<HolidayTypeEntity> HolidayTypes { get; set; }
