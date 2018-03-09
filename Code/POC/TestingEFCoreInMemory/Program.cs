ea3dc9cfc006e24d4a61002e597fb2b2aa295e04
schwarzie2478 <schwarzie2478@hotmail.com>
2018-03-09 01:49:30 +0100

    Added my own testing app for practice and documtation

---
+++ Code/POC/TestingEFCoreInMemory/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TestingEFCoreInMemory.Models;
+
+namespace TestingEFCoreInMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var db = new TimeRegistrationContext())
+            {
+                var user = new UserEntity();
+                user.FirstName = "Bob";
+                user.LastName = "Barker";
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                foreach( var savedUser in db.Users)
+                {
+                    Console.WriteLine($"user saved with data:  id={savedUser.Id}");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Code/POC/TestingEFCoreInMemory/TimeRegistrationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestingEFCoreInMemory.Models;
+
+namespace TestingEFCoreInMemory
+{
+    public class TimeRegistrationContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseInMemoryDatabase();
+        }
+    }
+}
