bd2f280991fb9bfdf1e33f67d9344b2d2f3498e4
Filip Nuyts <filipnuyts@skynet.be>
2018-03-17 12:48:15 +0100

    add migrations to seed users and to seed user userrole link

---
+++ Code/TimeRegistrationDemo/TimeRegistrationDemo.Data/Migrations/20180317113105_SeedUserUserRole.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeRegistrationDemo.Data.Migrations
+{
+    public partial class SeedUserUserRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlInsertUserUserRole(migrationBuilder, "John", "Doe", "E");
+            SqlInsertUserUserRole(migrationBuilder, "Adam", "Smith", "M");
+            SqlInsertUserUserRole(migrationBuilder, "Sarah", "Brown", "A");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //simply delete all records
+            migrationBuilder.Sql($"DELETE FROM {TimeRegistrationDbContext.DefaultSchema}.[UsersUserRoles]");
+        }
+
+        private void SqlInsertUserUserRole(MigrationBuilder migrationBuilder, string firstName, string lastName, string userRoleId)
+        {
+            var sqlSelectUser = $"SELECT [UserId] FROM {TimeRegistrationDbContext.DefaultSchema}.[Users] " +
+                                $"WHERE [FirstName] = '{firstName}' AND [LastName] = '{lastName}'";
+
+            migrationBuilder.Sql(
+                $"INSERT INTO {TimeRegistrationDbContext.DefaultSchema}.[UsersUserRoles] " +
+                $"([UserId],[UserRoleId]) " +
+                $"VALUES ( ({sqlSelectUser}), '{userRoleId}')");
+        }
+    }
+}
